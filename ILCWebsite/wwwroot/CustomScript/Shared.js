3d5f922f2cc2fd673ae767eadfc2da5f23676c8b
Ibrahim Elarabi <ebrahimelaraby71@gmail.com>
2023-10-22 02:07:40 +0300

    add preview image partial view

---
+++ ILCWebsite/wwwroot/CustomScript/Shared.js
@@ -0,0 +1,21 @@
+﻿const fileInput = document.getElementById("ImagePath");
+const imagePreview = document.getElementById("imagePreview");
+fileInput.addEventListener("change", function () {
+    const file = fileInput.files[0];
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            imagePreview.src = e.target.result;
+        };
+
+        reader.readAsDataURL(file);
+    } else {
+        imagePreview.src = ""; // Clear the preview if no file is selected
+    }
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+
+    console.log("DOM fully loaded and parsed");
+});
