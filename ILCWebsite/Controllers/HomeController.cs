7a8a41184860aa7f9ea4a409aea6b8a625d24a3e
mahmoudmakhaly123 <mahmoud.makhaly@totplatform.com>
2023-11-05 23:14:51 +0200

    fix issue

--- ILCWebsite/Controllers/HomeController.cs
+++ ILCWebsite/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Diagnostics;
 
 namespace ILCWebsite.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
