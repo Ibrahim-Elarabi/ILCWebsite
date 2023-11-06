4faba97213e170b2ab24a1d5d575974218cd8c1b
mahmoudmakhaly123 <mahmoud.makhaly@totplatform.com>
2023-11-06 12:46:59 +0200

    -change color

--- ILCWebsite/wwwroot/js/script.js
+++ ILCWebsite/wwwroot/js/script.js
@@ -14,7 +14,7 @@ function setLanguagePreference(lang) {
 function updateContent(langData) {
   document.querySelectorAll("[data-i18n]").forEach((element) => {
     const key = element.getAttribute("data-i18n");
-    element.textContent = langData[key];
+    element.textContent = langData[key]
   });
 }
 
