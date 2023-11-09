d5d23183c32e98959504b225be427fdb2b2a2376
Ibrahim Elarabi <ebrahimelaraby71@gmail.com>
2023-11-10 01:22:09 +0200

    Add Changes

---
+++ ILCWebsite/Areas/Admin/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ILCWebsite.Areas.Admin.Controllers
+{
+     [Area("Admin")]
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
