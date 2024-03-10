ac2e855915a7beb61df73ff3ac454a93b41f155d
Mahmoud Mohamed <MahmoudElgaml5115@gmail.com>
2024-03-10 19:07:42 +0200

    fix about us in adminstration

--- ILCWebsite/Areas/Admin/Controllers/AboutUsHomeController.cs
+++ ILCWebsite/Areas/Admin/Controllers/AboutUsHomeController.cs
@@ -23,88 +23,11 @@ namespace ILCWebsite.Areas.Admin.Controllers
 
         public IActionResult Index()
         {
-            var lst = _unitOfWork._aboutUsHomeService.GetAll();
-            var newList = _mapper.Map<List<AboutUsHomeVM>>(lst);
-            return View(newList.ToList());
-        }
-
-
-        public IActionResult Details(int id)
-        {
-            var aboutUs = _unitOfWork._aboutUsHomeService.FindOne(d => d.Id == id && d.IsDeleted != true);
+            var aboutUs = _unitOfWork._aboutUsHomeService.GetAll().FirstOrDefault();
             var result = _mapper.Map<AboutUsHomeVM>(aboutUs);
             return View(result);
-        }
-
-        public IActionResult Create()
-        {
-            return View(new CreateAboutUsHomeVM());
-        }
-        [HttpPost]
-        public async Task<JsonResult> Create(CreateAboutUsHomeVM model)
-        { 
-            if (!ModelState.IsValid)
-            {
-                return Json(model);
-            }
-            else
-            {
-                if (model.Image != null)
-                {
-                    try
-                    {
-                        var imagePath = _unitOfWork.UploadedFile(model.Image, "Images/Admin/Home");
-                        if (imagePath != null)
-                        {
-                            model.ImagePath = imagePath;
-                            var result = await _unitOfWork._aboutUsHomeService.InsertAsync(_mapper.Map<AboutUsHomeSection>(model));
-                            var checkSave = await _unitOfWork.CompleteAync();
-                            if (checkSave > 0)
-                            {
-                                return Json(new
-                                {
-                                    Success = true,
-                                    Message = "Item added successfully"
-                                });
-                            }
-                            else
-                            {
-                                return Json(new
-                                {
-                                    Success = false,
-                                    Message = "Failed to add item",
-                                });
-                            }
-                        }
-                        else
-                        {
-                            return Json(new
-                            {
-                                Success = false,
-                                Message = "Invalid ImagePath path to save image in it",
-                            });
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        return Json(new
-                        {
-                            Success = false,
-                            Message = ex.Message,
-                        });
-                    }
-                }
-                else
-                {
-                    return Json(new
-                    {
-                        Success = false,
-                        Message = "Empty image"
-                    });
-                } 
-            } 
-        }
-
+        } 
+         
         public async Task<IActionResult> Edit(int id)
         {
             var model = await _unitOfWork._aboutUsHomeService.GetByIdAsync(id);
@@ -156,51 +79,6 @@ namespace ILCWebsite.Areas.Admin.Controllers
                 });
             } 
         }
-
-        [HttpGet]
-        public JsonResult Delete(int id)
-        {
-            try
-            {
-                var model = _unitOfWork._aboutUsHomeService.GetById(id);
-                if (model != null)
-                {
-                    _unitOfWork._aboutUsHomeService.Delete(model);
-                    if (_unitOfWork.Complete() > 0)
-                    {
-                        return Json(new
-                        {
-                            Success = true,
-                            Message = "Item deleted successfully"
-                        });
-                    }
-                    else
-                    {
-                        return Json(new
-                        {
-                            Success = false,
-                            Message = "Failed to delete item"
-                        });
-
-                    }
-                }
-                else
-                {
-                    return Json(new
-                    {
-                        Success = false,
-                        Message = "No item found to remove"
-                    });
-                }
-            }
-            catch (Exception ex)
-            {
-                return Json(new
-                {
-                    success = false,
-                    Message = "An error occured , Please try again later," + ex.Message
-                });
-            }
-        }
+         
     }
 }
