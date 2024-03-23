77704c79b023e69451ff9e1a25bb84be40f27b50
Mahmoud Mohamed <MahmoudElgaml5115@gmail.com>
2024-03-23 04:10:24 +0200

    add document list

--- ILCWebsite/Controllers/DownloadController.cs
+++ ILCWebsite/Controllers/DownloadController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using ILC.BL.IRepo;
+using ILC.BL.Models.Admin.HomeSection.Downloads;
+using ILC.BL.Models.Admin.HomeSection.Service;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ILCWebsite.Controllers
 {
     public class DownloadController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        private readonly IMapper _mapper;
+        public DownloadController(IUnitOfWork unitOfWork,
+                                    IWebHostEnvironment hostingEnvironment,
+                                    IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _hostingEnvironment = hostingEnvironment;
+            _mapper = mapper;
+        }
         public IActionResult Index()
         {
-            return View();
-        } 
+            var documents = _unitOfWork._DownloadRepo.GetAll().ToList();
+            var result = _mapper.Map<List<DownloadVM>>(documents);
+            return View(result);
+        }
+        public ActionResult ViewFile(int id)
+        {
+            var download = _unitOfWork._DownloadRepo.GetById(id);
+            var downloadVM = _mapper.Map<DownloadVM>(download);
+            return View(downloadVM);
+        }
     }
 }
