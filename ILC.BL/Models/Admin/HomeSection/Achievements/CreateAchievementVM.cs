4e368eccffdb2b35c290381685d7d0a1817e71e7
Mahmoud Mohamed <MahmoudElgaml5115@gmail.com>
2024-06-02 09:57:27 +0300

    achivements enhancements

---
+++ ILC.BL/Models/Admin/HomeSection/Achievements/CreateAchievementVM.cs
@@ -0,0 +1,25 @@
+﻿using ILC.BL.Common.Mapping;
+using ILC.Domain.DBEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILC.BL.Models.Admin.HomeSection.Achievements
+{
+    public class CreateAchievementVM : IMapTo<Achievement>, IMapFrom<Achievement>
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? TitleEn { get; set; }
+
+        [Required]
+        public string? TitleAr { get; set; }
+
+        [Required]
+        public int? Value { get; set; }
+    }
+}
--- ILCWebsite/Areas/Admin/Controllers/AchievementController.cs
+++ ILCWebsite/Areas/Admin/Controllers/AchievementController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using ILC.BL.IRepo; 
 using ILC.BL.Models.Admin.HomeSection.Achievements;
+using ILC.BL.Models.Admin.HomeSection.Blog;
 using ILC.Domain.DBEntities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -19,17 +20,84 @@ namespace ILCWebsite.Areas.Admin
             _unitOfWork = unitOfWork; 
             _mapper = mapper;
         }
+
         public IActionResult Index()
         {
             var lst = _unitOfWork._AchievementRepo.GetAll();
             var newList = _mapper.Map<List<AchievementVM>>(lst);
             return View(newList.ToList());
         }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new CreateAchievementVM());
+        }
+         
+        [HttpPost]
+        public async Task<JsonResult> Create(CreateAchievementVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Json(model);
+            }
+            else
+            { 
+                try
+                {
+                    var OldElementsCount = _unitOfWork._AchievementRepo.GetAll().Count();
+                    if (OldElementsCount < 5)
+                    {
+                        var result = await _unitOfWork._AchievementRepo.InsertAsync(_mapper.Map<Achievement>(model));
+                        var checkSave = await _unitOfWork.CompleteAync();
+                        if (checkSave > 0)
+                        {
+                            return Json(new
+                            {
+                                Success = true,
+                                Message = "Item added successfully"
+                            });
+                        }
+                        else
+                        {
+                            return Json(new
+                            {
+                                Success = false,
+                                Message = "Failed to add item",
+                            });
+                        }
+                    }
+                    else
+                    {
+                        return Json(new
+                        {
+                            Success = false,
+                            Message = "Max number of Achivements you can create is five elements",
+                        });
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    return Json(new
+                    {
+                        Success = false,
+                        Message = ex.Message,
+                    });
+                } 
+            }
+        }
+
+
+
+
         public async Task<IActionResult> Edit(int id)
         {
             var model = await _unitOfWork._AchievementRepo.GetByIdAsync(id);
             return View(_mapper.Map<EditAchievementVM>(model));
         }
+         
         [HttpPost]
         public async Task<JsonResult> Edit(EditAchievementVM model)
         {
@@ -72,5 +140,52 @@ namespace ILCWebsite.Areas.Admin
             }
         }
 
+
+
+        [HttpGet]
+        public JsonResult Delete(int id)
+        {
+            try
+            {
+                var model = _unitOfWork._AchievementRepo.GetById(id);
+                if (model != null)
+                {
+                    _unitOfWork._AchievementRepo.Delete(model);
+                    if (_unitOfWork.Complete() > 0)
+                    {
+                        return Json(new
+                        {
+                            Success = true,
+                            Message = "item deleted successfully"
+                        });
+                    }
+                    else
+                    {
+                        return Json(new
+                        {
+                            Success = false,
+                            Message = "Failed to delete item"
+                        });
+
+                    }
+                }
+                else
+                {
+                    return Json(new
+                    {
+                        Success = false,
+                        Message = "No item found to remove"
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    success = false,
+                    Message = "An error occured , Please try again later," + ex.Message
+                });
+            }
+        }
     }
 }
