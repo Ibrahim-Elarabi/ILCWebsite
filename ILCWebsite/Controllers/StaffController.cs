5d12f1fdcad5a474f178e49d1ef9ff6a25d6fb82
Mahmoud Mohamed <MahmoudElgaml5115@gmail.com>
2024-06-08 01:50:59 +0300

    adding staff list page

---
+++ ILCWebsite/Controllers/StaffController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using ILC.BL.IRepo;  
+using ILC.BL.Models.Admin.HomeSection.Staff;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ILCWebsite.Controllers
+{
+    public class StaffController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        private readonly IMapper _mapper;
+        public StaffController(IUnitOfWork unitOfWork,
+                                    IWebHostEnvironment hostingEnvironment,
+                                    IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _hostingEnvironment = hostingEnvironment;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            var staffs = _unitOfWork._staffHomeRepo.GetAll().OrderBy(d => d.Order).ToList();
+            var result = _mapper.Map<List<StaffHomeVM>>(staffs);
+            return View(result);
+        } 
+    }
+}
